6151894dc50787949493d1236eb8f5716f862786
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2022-12-16 13:21:37 -0600

    no cuda on macbook

--- petsc/all_installs.sh
+++ petsc/all_installs.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-version=3.18.1
+version=3.18.2
 if [ $# -eq 1 ] ; then 
   version=$1
 fi
 
-options="P4P=1 KOKKOS=1"
-module load cuda
-for d in 1 0 ; do
-    make install DEBUG=$d PACKAGEVERSION=$version \
-	$options CUDA=1
-done
+if [ "${TACC_SYSTEM}" != "macbookair" ] ; then
+    options="P4P=1 KOKKOS=1"
+    module load cuda
+    for d in 1 0 ; do
+	make install DEBUG=$d PACKAGEVERSION=$version \
+	     $options CUDA=1
+    done
+    module unload cuda
+fi
 
-options="HDF5=1 METIS=1 P4P=1 KOKKOS=1 SLEPC=1"
-module unload cuda
+options="HDF5=1 METIS=1 P4P=1 KOKKOS=1 SLEPC=1 FFTW=1"
 for d in 1 0 ; do
     # make with default official version
     make install DEBUG=$d PACKAGEVERSION=$version \
