ac9779a51c8de066bd408298829098cef6b3ca38
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2022-12-23 08:17:57 -0600

    custom settings

---
+++ trilinos/s2_oneapi.sh
@@ -0,0 +1,6 @@
+module unload python2
+module use /scratch/projects/compilers/modulefiles
+module load oneapi/22.3.0 oneapi21/impi
+export TACC_MKL_DIR=/scratch/projects/compilers/oneapi_2022.3.0/mkl/latest
+export TACC_MKL_INC=/scratch/projects/compilers/oneapi_2022.3.0/mkl/latest/include
+export TACC_MKL_LIB=/scratch/projects/compilers/oneapi_2022.3.0/mkl/latest/lib/intel46
