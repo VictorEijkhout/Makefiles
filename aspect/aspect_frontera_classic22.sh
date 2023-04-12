e412544230a8aa40b4b22b6616bb1add46034164
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-04-12 15:43:41 -0500

    working on scripts

--- aspect/aspect_frontera_classic22.sh
+++ aspect/aspect_frontera_classic22.sh
@@ -8,17 +8,18 @@ m=100
 function usage() {
     echo "Usage: $0 [ -h ] [ -x ] [ -j nnn ] [ -l ] nnn"
     echo "where nnn:"
-    echo " 1: petsc"
-    echo " 2: p4est"
-    echo " 3: boost"
-    echo " 4: pcre"
-    echo " 5: swig"
-    echo " 6: trilinos"
-    echo " 7: dealii"
-    echo " 8: aspect"
+    i=1
+    for p in petsc p4est boost pcre swig trilinos dealii aspect ; do 
+	echo " $i : $p"
+	#module avail $p 2>&1 | awk 'NF>0 {print}'
+	i=$(( i+1 ))
+    done
 }
 
+source ../frontera_env_classic22.sh >/dev/null 2>&1
+
 if [ $# -eq 0 ] ; then 
+    module list
     usage
     exit 0
 fi
@@ -37,7 +38,7 @@ while [ $# -gt 0 ] ; do
     elif [ "$1" = "-x" ] ; then 
 	setx=1; shift
     elif [ "$1" = "-j" ] ; then 
-	shift; jcount = $1; shift
+	shift; jcount=$1; shift
     else
 	m=$1; shift
     fi
@@ -49,7 +50,6 @@ if [ $setx -gt 0 ] ; then
 fi
 
 ## LMOD_SH_DBG_ON=1
-source ../frontera_env_classic22.sh
 
 echo "================ Starting installation with modules:"
 module list
@@ -85,88 +85,3 @@ for ixy in \
     fi
 done 
 
-exit 0
-
-x=petsc
-y=3.18.3
-if [ $list -eq 1 ] ; then 
-    module_avail $x $y
-fi
-if [ $m -eq 1 ] ; then 
-    ( cd $x && make configure build public JCOUNT=${jcount} PACKAGEVERSION=$y )
-    exit 0
-fi
-module load $x/$y
-if [ $? -ne 0 ] ; then echo "Could not load $x" && exit 1 ; fi
-
-x=p4est
-y=2.8
-if [ $list -eq 1 ] ; then 
-    module_avail $x $y
-fi
-if [ $m -eq 2 ] ; then 
-    ( cd $x && make configure build public JCOUNT=${jcount} PACKAGEVERSION=$y )
-    exit 0
-fi
-module load $x/$y
-if [ $? -ne 0 ] ; then echo "Could not load $x" && exit 1 ; fi
-
-x=boost
-y=1.81.0
-if [ $list -eq 1 ] ; then 
-    module_avail $x $y
-fi
-if [ $m -eq 3 ] ; then 
-    ( cd $x && make configure build public JCOUNT=${jcount} PACKAGEVERSION=$y )
-    exit 0
-fi
-module load $x/$y
-if [ $? -ne 0 ] ; then echo "Could not load $x" && exit 1 ; fi
-
-x=pcre2
-y=git
-if [ $list -eq 1 ] ; then 
-    module_avail $x $y
-fi
-if [ $m -eq 4 ] ; then 
-    ( cd $x && make configure build public JCOUNT=${jcount} PACKAGEVERSION=$y )
-    exit 0
-fi
-module load $x/$y
-if [ $? -ne 0 ] ; then echo "Could not load $x" && exit 1 ; fi
-
-x=swig
-y=4.1.1
-if [ $list -eq 1 ] ; then 
-    module_avail $x $y
-fi
-if [ $m -eq 5 ] ; then 
-    ( cd $x && make configure build public JCOUNT=${jcount} PACKAGEVERSION=$y )
-    exit 0
-fi
-module load $x/$y
-if [ $? -ne 0 ] ; then echo "Could not load $x" && exit 1 ; fi
-
-x=trilinos
-y=13.4.1
-if [ $list -eq 1 ] ; then 
-    module_avail $x $y
-fi
-if [ $m -eq 6 ] ; then 
-    ( cd $x && make configure build public JCOUNT=${jcount} PACKAGEVERSION=$y PYTHON=OFF )
-    exit 0
-fi
-module load $x/$y
-if [ $? -ne 0 ] ; then echo "Could not load $x" && exit 1 ; fi
-
-x=dealii
-y=9.4.1
-if [ $list -eq 1 ] ; then 
-    module_avail $x $y
-fi
-if [ $m -eq 7 ] ; then 
-    ( cd $x && make build public JCOUNT=${jcount} PACKAGEVERSION=$y )
-    exit 0
-fi
-module load $x/$y
-if [ $? -ne 0 ] ; then echo "Could not load $x" && exit 1 ; fi
