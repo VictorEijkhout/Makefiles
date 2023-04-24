fbab1fafe88e3d90727e054095a34e7debd9e494
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-04-24 09:41:35 -0500

    package install scripts

---
+++ env_frontera_classic18.sh
@@ -0,0 +1,20 @@
+echo "module reset"
+module purge
+module reset
+module list
+
+echo "module unload intel and others"
+module unload intel oneapi impi python3
+module list
+
+echo "adding experimental and my own module paths"
+## THIS CONTAINS CRAP module use /scratch1/projects/compilers/modulefiles
+export MODULEROOT=${WORK}/modulefiles
+export MY_MODULEPATH_ROOT=${MODULEROOT}
+module use ${MY_MODULEPATH_ROOT}/Core
+
+intelversion=18.0.5
+echo "loading intel ${intelversion}"
+module load intel/${intelversion} impi/${intelversion}
+module load python3
+module list
