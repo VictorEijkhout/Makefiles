317385f9d456470ab57fab70cd2e0da1e1d4918e
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-04-25 09:48:06 -0500

    s2 1api

---
+++ env_stampede2_oneapi.sh
@@ -0,0 +1,18 @@
+module purge
+module reset
+module unload intel oneapi impi python2 python3
+
+module use /scratch/projects/compilers/modulefiles
+
+export MODULEROOT=${WORK}/modulefiles
+export MY_MODULEPATH_ROOT=${MODULEROOT}
+
+module use ${MY_MODULEPATH_ROOT}/Core
+
+intelversion=21.4.0
+module load oneapi/${intelversion}
+module load impi/${intelversion}
+
+# module load intel22/python3
+
+module list
