53c55fd1b5493ec71704152eb5600b5f560a7e96
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-05-12 08:28:31 -0500

    s2 configurations

---
+++ env_stampede2_intel19.sh
@@ -0,0 +1,16 @@
+module reset
+module unload intel oneapi gcc impi python2 python3
+
+## module use /scratch/projects/compilers/modulefiles
+
+export MODULEROOT=${WORK}/modulefiles
+export MY_MODULEPATH_ROOT=${MODULEROOT}
+
+module use ${MY_MODULEPATH_ROOT}/Core
+
+module load intel/19.1.1
+module load impi/19.0.9
+
+# module load intel22/python3
+
+module list
