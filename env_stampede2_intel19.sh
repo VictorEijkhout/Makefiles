0549271fd46bb3cce2654ad09478e4c92b4380fe
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-06-23 15:30:42 -0500

    made portable

--- env_stampede2_intel19.sh
+++ env_stampede2_intel19.sh
@@ -3,7 +3,8 @@ module unload intel oneapi gcc impi python2 python3 2>/dev/null
 
 ## module use /scratch/projects/compilers/modulefiles
 
-export MODULEROOT=${WORK}/modulefiles
+export VICTOR_WORK=/work2/00434/eijkhout/stampede2
+export MODULEROOT=${VICTOR_WORK}/modulefiles
 export VICTOR_MODULEPATH_ROOT=${MODULEROOT}
 
 module use ${VICTOR_MODULEPATH_ROOT}/Core
--- env_stampede2_intel22.sh
+++ env_stampede2_intel22.sh
@@ -3,7 +3,7 @@ module unload intel oneapi gcc impi python2 python3 2>/dev/null
 
 module use /scratch/projects/compilers/modulefiles
 
-VICTOR_WORK=/work2/00434/eijkhout/stampede2
+export VICTOR_WORK=/work2/00434/eijkhout/stampede2
 export MODULEROOT=${VICTOR_WORK}/modulefiles
 export VICTOR_MODULEPATH_ROOT=${MODULEROOT}
 
