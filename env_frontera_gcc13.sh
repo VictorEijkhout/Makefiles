2c46832e8f059c8fc07de94468023d0692e0ac00
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-09-01 15:49:23 -0500

    gcc 13

---
+++ env_frontera_gcc13.sh
@@ -0,0 +1,20 @@
+echo "module reset"
+module purge
+module reset
+## module list
+
+echo "module unload intel and others"
+module unload intel oneapi impi python3 2>/dev/null
+## module list
+
+echo "adding experimental and my own module paths"
+export MODULEROOT=/work2/00434/eijkhout/frontera/modulefiles
+export VICTOR_MODULEPATH_ROOT=${MODULEROOT}
+module use ${VICTOR_MODULEPATH_ROOT}/Core
+
+gnuversion=13.2.0
+echo "loading gnu ${gnuversion}"
+module load gcc/${gnuversion} 
+## module load impi/21.9.0
+#module load python3
+module list
