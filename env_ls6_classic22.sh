ae20832c78ed85e028037968dbceb5a3a10fa0c4
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-10-10 10:08:12 -0500

    made ls6 settings portable

--- env_ls6_classic22.sh
+++ env_ls6_classic22.sh
@@ -3,7 +3,8 @@ module unload intel gcc impi python3 python2 2>/dev/null
 
 module use /scratch/projects/compilers/modulefiles
 
-export MODULEROOT=${WORK}/modulefiles
+export VICTOR_WORK=/work2/00434/eijkhout/ls6
+export MODULEROOT=${VICTOR_WORK}/modulefiles
 export VICTOR_MODULEPATH_ROOT=${MODULEROOT}
 
 module use ${VICTOR_MODULEPATH_ROOT}/Core
--- env_ls6_gcc12.sh
+++ env_ls6_gcc12.sh
@@ -14,7 +14,8 @@ module unload intel oneapi gcc impi mvapich2 python3 python2 2>/dev/null 2>/dev/
 
 echo "adding experimental and my own module paths"
 ## module use /scratch/projects/compilers/modulefiles
-export MODULEROOT=${WORK}/modulefiles
+export VICTOR_WORK=/work2/00434/eijkhout/ls6
+export MODULEROOT=${VICTOR_WORK}/modulefiles
 export VICTOR_MODULEPATH_ROOT=${MODULEROOT}
 module use ${VICTOR_MODULEPATH_ROOT}/Core
 
