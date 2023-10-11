f57b3620d81d7cd53094181022a2c06a9b6ba023
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-10-11 13:25:49 -0500

    gcc13 settings

---
+++ env_ls6_gcc13.sh
@@ -0,0 +1,29 @@
+##
+## load OneAPI intel compiler
+##
+
+echo "module reset"
+module purge
+module reset
+# module list
+module load TACC
+
+echo "module unload intel and others"
+module unload intel oneapi gcc impi mvapich2 python3 python2 2>/dev/null 2>/dev/null
+# module list
+
+echo "adding experimental and my own module paths"
+## module use /scratch/projects/compilers/modulefiles
+export VICTOR_WORK=/work2/00434/eijkhout/ls6
+export MODULEROOT=${VICTOR_WORK}/modulefiles
+export VICTOR_MODULEPATH_ROOT=${MODULEROOT}
+module use ${VICTOR_MODULEPATH_ROOT}/Core
+
+gccversion=13.2.0
+echo "loading gcc ${gccversion}"
+module load gcc/${gccversion}
+echo "WARNING no impi available for this compiler"
+##module load impi/19.0.9
+module load python3
+
+module list
