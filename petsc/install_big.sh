627b040027420fe0ed60d728dbeb681a25898372
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-10-21 00:13:27 -0500

    version parameter

--- petsc/install_big.sh
+++ petsc/install_big.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 jcount=6
+pversion=3.20.0
 function usage() {
-    echo "Usage: $0 [ -h ] [ -j 123 ] [ -v pversion ]"
+    echo "Usage: $0 [ -h ] [ -j 123 ] [ -v (default: ${pversion} ]"
 }
 
-pversion=3.20.0
 while [ $# -gt 0 ] ; do
     if [ $1 = "-h" ] ; then
 	usage && exit 0
@@ -29,14 +29,14 @@ module load phdf5/1.14
 module load cuda/12
 
 export biglog=big_install.log
-for d in 0 ; do 
-    make --no-print-directory biginstall EXT= JCOUNT=${jcount} PACKAGEVERSION=${pversion} \
+for d in 0 1 ; do 
+    make --no-print-directory biginstall JCOUNT=${jcount} PACKAGEVERSION=${pversion} \
 	 DEBUG=$d \
 	 EIGEN=1 FFTW3=1 HDF5=1 HYPRE=0 \
 	 CUDA=0 FORTRAN=0 \
 	 PETSC4PY=1 SLEPC4PY=1 \
 	 2>&1 | tee -a ${biglog}
-    make --no-print-directory biginstall EXT= JCOUNT=${jcount} PACKAGEVERSION=${pversion} \
+    make --no-print-directory biginstall JCOUNT=${jcount} PACKAGEVERSION=${pversion} \
 	 DEBUG=$d \
 	 CUDA=1 FORTRAN=0 \
 	 2>&1 | tee -a ${biglog}
