a6868c088e1b70fe4970be5a6635957a194c2330
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-11-10 13:10:47 -0600

    version parameter

--- all_compilers_install.sh
+++ all_compilers_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -lt 1 -o "$1" = "-h" ] ; then 
-    echo "Usage: $0 [ -j 123 ] [ -m \"m1 m2 m3\" ] [ -t target ] package"
+    echo "Usage: $0 [ -j 123 ] [ -m \"m1 m2 m3\" ] [ -t target ] [ -v version ] package"
     echo "    -m : modules to be loaded"
     echo "    -t : make target or default_install"
     exit 1
@@ -10,6 +10,7 @@ fi
 jcount=4
 modules=
 target=default_install
+version=
 while [ $# -gt 1 ] ; do
     if [ "$1" = "-j" ] ; then 
 	shift && jcount=$1 && shift
@@ -17,6 +18,8 @@ while [ $# -gt 1 ] ; do
 	shift && modules=$1 && shift
     elif [ "$1" = "-t" ] ; then
 	shift && target=$1 && shift
+    elif [ "$1" = "-v" ] ; then
+	shift && version=$1 && shift
     fi
 done
 package=$1
@@ -32,6 +35,7 @@ for compiler in intel19 intel23 gcc9 gcc13 ; do
 	&& module list \
 	&& cd $package \
 	&& make JCOUNT=${jcount} ${target} \
+	    $( if [ ! -z ${version} ] ; then echo PACKAGEVERSION=${version} ; fi ) \
     )
 done 2>&1 | tee all_${package}.log
 echo && echo "See: all_${package}.log" && echo
