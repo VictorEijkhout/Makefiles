0debd92f71d07f2c163f08c2caeb6428af02a105
eijkhout <eijkhout@staff.stampede3.tacc.utexas.edu>
2024-01-03 11:41:00 -0600

    yet another machine

---
+++ env_s3_intel24.sh
@@ -0,0 +1,33 @@
+##
+## load OneAPI intel compiler
+##
+
+echo "module reset"
+module purge
+module reset
+# module list
+module load TACC
+
+echo "module unload intel and others"
+module unload intel oneapi gcc impi mvapich2 python3 python2 2>/dev/null
+# module list
+
+echo "adding experimental and my own module paths"
+## ONEAPI IS not yet OFFICIAL as INTEL
+module use /scratch/projects/compilers/modulefiles
+
+export VICTOR_WORK=/work2/00434/eijkhout/ls6
+export MODULEROOT=${VICTOR_WORK}/modulefiles
+
+export VICTOR_MODULEPATH_ROOT=${MODULEROOT}
+module use ${VICTOR_MODULEPATH_ROOT}/Core
+
+intelversion=23.1.0
+echo "loading intel ${intelversion}"
+module load intel/${intelversion}
+export TACC_CC=icx
+export TACC_CXX=icpx
+export TACC_FC=ifx
+module load impi/21.9.0
+
+module list
