203169793579fefa08e89922862840c798339f20
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-06-09 11:31:22 -0500

    ncl scripts

---
+++ nclncarg/ncl_gcc.sh
@@ -0,0 +1,14 @@
+export CC=gcc
+export CFLAGS="-O2 -ansi -std=c99 -fopenmp -fPIC"
+export CCOPTIONS="-O2 -ansi -std=c99 -fopenmp -fPIC"
+
+export CXX=g++
+export CXXFLAGS="-O2 -ansi -std=c99 -fopenmp -fPIC"
+export CPPFLAGS="-DNDEBUG"
+
+export FC=gfortran
+export FFLAGS="-O2 -fPIC -fopenmp -fallow-argument-mismatch"
+
+export F90=gfortran
+export F90FLAGS="-O2 -fPIC -fopenmp -fallow-argument-mismatch"
+
