ecde6dd74f206ccf48f87f34abfdcfb8ce6dae90
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-06-10 07:14:53 -0500

    env settings only in compiler specific files

--- nclncarg/ncl_icx.sh
+++ nclncarg/ncl_icx.sh
@@ -7,7 +7,8 @@ export CXXFLAGS="-O2 -ansi -std=c99 -fopenmp -fPIC"
 export CPPFLAGS="-DNDEBUG"
 
 export FC=ifx
-export FFLAGS="-O2 -fPIC -fopenmp -fallow-argument-mismatch"
+export FFLAGS='-O2 -fPIC -fopenmp -fallow-argument-mismatch -fallow-invalid-boz'
+export FCOPTIONS='-O2 -fPIC -fopenmp -fallow-argument-mismatch -fallow-invalid-boz'
 
 export F90=ifx
 export F90FLAGS="-O2 -fPIC -fopenmp -fallow-argument-mismatch"
--- nclncarg/nclncarg_configure.sh
+++ nclncarg/nclncarg_configure.sh
@@ -31,19 +31,6 @@ echo "================ Installation:"
 echo "    into ${installation}"
 echo "    using tmp=${tmp}"
 
-echo "================ VLE set environment"
-export CC=gcc
-export CFLAGS='-O2 -ansi -std=c99 -fopenmp -fPIC'
-export CCOPTIONS='-O2 -ansi -std=c99 -fopenmp -fPIC'
-export FC=gfortran
-export FFLAGS='-O2 -fPIC -fopenmp -fallow-argument-mismatch -fallow-invalid-boz'
-export FCOPTIONS='-O2 -fPIC -fopenmp -fallow-argument-mismatch -fallow-invalid-boz'
-export F90=gfortran
-export F90FLAGS='-O2 -fPIC -fopenmp'
-export CXX=g++
-export CXXFLAGS='-O2 -ansi -std=c99 -fopenmp -fPIC'
-export CPPFLAGS='-DNDEBUG'
-
 echo "================ VLE edit Linux settings"
 sed -i config/LINUX \
     -e "/FcOptions/s/\$/ -O2 -fallow-argument-mismatch -fallow-invalid-boz/"
