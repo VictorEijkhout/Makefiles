ff5bbe6b341bc8e2914bc443153b9f8a59faf9be
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-06-10 07:10:14 -0500

    icx settings

---
+++ nclncarg/ncl_icx.sh
@@ -0,0 +1,14 @@
+export CC=icx
+export CFLAGS="-O2 -ansi -std=c99 -fopenmp -fPIC"
+export CCOPTIONS="-O2 -ansi -std=c99 -fopenmp -fPIC"
+
+export CXX=icpx
+export CXXFLAGS="-O2 -ansi -std=c99 -fopenmp -fPIC"
+export CPPFLAGS="-DNDEBUG"
+
+export FC=ifx
+export FFLAGS="-O2 -fPIC -fopenmp -fallow-argument-mismatch"
+
+export F90=ifx
+export F90FLAGS="-O2 -fPIC -fopenmp -fallow-argument-mismatch"
+
