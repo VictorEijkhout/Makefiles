2fbc3b47235a83bcf18b835eb8f2e7aa6906a46c
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-11-26 18:28:29 -0600

    terse

--- env_ls6_intel24.sh
+++ env_ls6_intel24.sh
@@ -2,19 +2,17 @@
 ## load OneAPI intel compiler
 ##
 
-echo "module reset"
+echo "==== module reset"
 module purge
-module reset
+module reset 2>/dev/null
 # module list
 module load TACC
 
-echo "module unload intel and others"
+echo "==== module unload intel and others"
 module unload intel oneapi gcc impi mvapich2 python3 python2 2>/dev/null
 # module list
 
-echo "adding experimental and my own module paths"
-## ONEAPI IS not yet OFFICIAL as INTEL
-## module use /scratch/projects/compilers/modulefiles
+echo "==== adding experimental and my own module paths"
 
 export VICTOR_WORK=/work2/00434/eijkhout/ls6
 export MODULEROOT=${VICTOR_WORK}/modulefiles
@@ -23,7 +21,7 @@ export VICTOR_MODULEPATH_ROOT=${MODULEROOT}
 module use ${VICTOR_MODULEPATH_ROOT}/Core
 
 intelversion=24.1
-echo "loading intel ${intelversion}"
+echo "==== loading intel ${intelversion}"
 module load intel/${intelversion}
 # export TACC_CC=icx
 # export TACC_CXX=icpx
@@ -33,4 +31,4 @@ module load impi/21.12
 ## let's see if this python is compatible enough
 ## module load oneapi22/python3/3.9.10
 
-module list
+module -t list
