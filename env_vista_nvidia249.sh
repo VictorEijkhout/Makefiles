1ad392a10a81abbc771b9c07c551656d1541986b
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-03-11 13:54:04 -0500

    cuda build fails internal compiler error

--- env_vista_nvidia249.sh
+++ env_vista_nvidia249.sh
@@ -10,4 +10,5 @@ module reset
 export MODULEPATH=${WORK}/modulefiles/Core:${MODULEPATH}
 module load nvidia/24.9
 module load openmpi/5.0.5_nvc249
-module load nvpl
+module load nvpl nvidia_math
+#module load nvhpc-hpcx
