41ed2622ea5d9d028828e4374b8f2747344ab551
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-03-12 08:39:07 -0500

    nvidia fake python install

--- env_vista_nvidia249.sh
+++ env_vista_nvidia249.sh
@@ -12,3 +12,10 @@ module load nvidia/24.9
 module load openmpi/5.0.5_nvc249
 module load nvpl nvidia_math
 #module load nvhpc-hpcx
+
+##
+## python hack
+##
+export TACC_PYTHON_DIR=/opt/apps/gcc14/cuda12/python3/3.11.8
+export PATH=${PATH}:/opt/apps/gcc14/cuda12/python3/3.11.8/bin/
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${TACC_PYTHON_DIR}/lib
