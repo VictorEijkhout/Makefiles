82dec8ce7422f898e2d508a411cdecea222aed4e
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-05-13 09:06:13 -0500

    trilinos fixes

--- ladder.sh
+++ ladder.sh
@@ -30,6 +30,29 @@ if [ $# -eq 0 ] ; then
     exit 0
 fi
 
+function parse_numpacver {
+    numpacver="$1"
+    # number in the list
+    num=${numpacver%%,*}
+    # package and optional version
+    pacver=${numpacver#*,}
+    # package
+    package=${pacver%,*}
+    # directory, could be package name or different
+    eval packagedir=\${${package}_dir}
+    if [ -z "${packagedir}" ] ; then packagedir=${package} ; fi
+    # version could be empty
+    version=$( pacver="${pacver}," && echo ${pacver#*,} | tr -d ',' )
+    if [ ! -z $version ] ; then 
+	eval fullversion=\${${package}_full_version}
+	if [ -z "$fullversion" ] ; then fullversion=${version} ; fi
+    else 
+	# get default from makefile
+	fullversion=$( cd ../${packagedir} && make --no-print-directory version )
+	version=${fullversion}
+    fi
+}
+
 function module_avail {
     # $1=package $2=test version $3=install version
     if [ -z "$2" ] ; then 
@@ -89,45 +112,29 @@ if [ ! -f "${settings}" ] ; then
     echo "Error: no such settings file <<${settings}>>" && exit 1 ; fi
 source ${settings} >/dev/null 2>&1
 
+if [ "${packages}" = "0" -a -z "${list}" ] ; then 
+  exit 0
+fi
+
 if [ $setx -gt 0 ] ; then 
     set -x
 fi
 
-echo "================ Starting installation with modules:"
-module -t list 2>&1 | sort | tr '\n' ' ' && echo
-
-if [ "${packages}" = "0" -a -z "${list}" ] ; then 
-  exit 0
-fi
+ladderlog=ladder_${TACC_FAMILY_COMPILER}${TACC_FAMILY_COMPILER_VERSION}.log
+( \
+  echo "================ Starting installation with modules:" \
+      && module -t list 2>&1 | sort | tr '\n' ' ' && echo \
+  ) | tee ${ladderlog}
 
 if [ -z "${list}" ] ; then
     echo "---------------- installing packages: ${packages}"
 else
     echo "---------------- listing packages"
 fi
-ladderlog=ladder_${TACC_FAMILY_COMPILER}${TACC_FAMILY_COMPILER_VERSION}.log
 for m in $( echo "${packages}" | tr , ' ' ) ; do
     for numpacver in ${numladder} \
 	       ; do \
-	# number in the list
-	num=${numpacver%%,*}
-	# package and optional version
-	pacver=${numpacver#*,}
-	# package
-	package=${pacver%,*}
-	# directory, could be package name or different
-	eval packagedir=\${${package}_dir}
-	if [ -z "${packagedir}" ] ; then packagedir=${package} ; fi
-	# version could be empty
-	version=$( pacver="${pacver}," && echo ${pacver#*,} | tr -d ',' )
-	if [ ! -z $version ] ; then 
-	    eval fullversion=\${${package}_full_version}
-	    if [ -z "$fullversion" ] ; then fullversion=${version} ; fi
-	else 
-	    # get default from makefile
-	    fullversion=$( cd ../${packagedir} && make --no-print-directory version )
-	    version=${fullversion}
-	fi
+	parse_numpacver "${numpacver}"
 	echo "================"
 	echo "Package $num: $package version $version"
 	if [ ! -z "${list}" ] ; then 
@@ -142,11 +149,11 @@ for m in $( echo "${packages}" | tr , ' ' ) ; do
 	    if [ $? -ne 0 ] ; then echo "Could not load $package" && exit 1 ; fi
 	    PACKAGE=$( echo ${package} | tr a-z -A-Z )
 	    module -t list $package/$version 
-	    module show $package/$version 2>&1 | \grep DIR
-	    eval echo " .. loaded ${package}/${version} at \${TACC_${PACKAGE}_DIR}"
+	    #module show $package/$version 2>&1 | \grep DIR
+	    eval echo " .. loaded ${package}/${version} at \${TACC_${package}_DIR}"
 	fi
     done 
-done 2>&1 | tee ${ladderlog}
+done 2>&1 | tee -a ${ladderlog}
 
 echo && echo "See: ${ladderlog}" && echo
 
