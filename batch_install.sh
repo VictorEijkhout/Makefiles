7d4ace7dacae98633b19ae8ef3ce477c61db1077
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-10-13 14:56:48 -0500

    long running time

--- batch_install.sh
+++ batch_install.sh
@@ -5,19 +5,19 @@ package=$1
 if [ $# -eq 2 ] ; then
     queue=$2
 else
-    queue=development
+    queue=normal
 fi
 
 cat >install.slurm <<EOF
 #!/bin/bash
 
-#SBATCH -J installjob           # Job name
-#SBATCH -o installjob.o%j       # Name of stdout output file
-#SBATCH -e installjob.o%j       # Name of stderr error file
+#SBATCH -J install${package}           # Job name
+#SBATCH -o install${package}.o%j       # Name of stdout output file
+#SBATCH -e install${package}.o%j       # Name of stderr error file
 #SBATCH -p ${queue}             # Queue (partition) name
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -t 02:0:0
+#SBATCH -t 4:0:0
 ##SBATCH --mail-user=myname@myschool.edu
 ##SBATCH --mail-type=all    # Send email at begin and end of job
 #SBATCH -A A-ccsc       # Allocation name (req'd if you have more than 1)
