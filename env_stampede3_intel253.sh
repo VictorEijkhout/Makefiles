2d61130ff1d34b7adbcc5f8a0383f896b9c13017
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-11-19 08:03:01 -0600

    hdf5 2.0 unfindable

---
+++ env_stampede3_intel253.sh
@@ -0,0 +1,38 @@
+##
+## load OneAPI intel compiler
+##
+
+echo "module reset"
+module purge
+module reset 2>/dev/null
+module load TACC
+
+echo "module unload intel and others"
+module unload intel oneapi gcc impi mvapich2 python3 python2 2>/dev/null
+
+## 25 is on scratch
+## no longer?
+module use /scratch/projects/compilers/modulefiles
+
+export VICTOR_WORK=/work2/00434/eijkhout/stampede3
+export MODULEROOT=${VICTOR_WORK}/modulefiles
+export VICTOR_MODULEPATH_ROOT=${VICTOR_WORK}/modulefiles
+
+module use ${VICTOR_MODULEPATH_ROOT}/Core
+export MODULEPATH=$( splitpath MODULEPATH | grep -v intel24 | assemblepath )
+# splitpath MODULEPATH
+
+intelversion=25.3
+echo "Loading intel ${intelversion}"
+module load intel/${intelversion}
+module load impi
+
+echo && echo "Module path:"
+# why is this needed?
+export MODULEPATH=$( splitpath MODULEPATH | grep -v intel24 | assemblepath )
+splitpath MODULEPATH
+
+echo && echo "Loaded:"
+module -t list 2>&1 | sort | tr '\n' ' ' 
+echo
+
