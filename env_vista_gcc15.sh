866f0299f01f512cd6cc6199fecf7908a9302fbb
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2026-04-28 12:22:59 -0500

    kokkos 5.1.1

--- env_vista_gcc15.sh
+++ env_vista_gcc15.sh
@@ -1,8 +1,8 @@
 ## module load lmod
 module -t reset 2>/dev/null
 module -t use ${WORK}/modulefiles/Core
-module -t load gcc/15.1 2>/dev/null
-module -t load openmpi/5.0.8 2>/dev/null
+module -t load gcc/15.1 0>/dev/null
+module -t load openmpi 2>/dev/null
 module -t load cuda nvpl nvidia_math 2>/dev/null
 module -t list 2>&1 | sort | tr '\n' ' '
 echo
