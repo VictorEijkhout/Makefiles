0c86282375c603df13ab6c2b187cd81122c073db
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2026-07-12 16:20:32 -0500

    trilinos 17.1.1

--- bin/package_avail.sh
+++ bin/package_avail.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+function usage () {
+    echo "Usage: $0 [ -h ] [ p1 [ p2 [ p3 ..."
+    echo "  try: $0 \$( packages.sh )"
+}
+if [ $# -eq 0 -o "$1" = "-h" ] ; then
+    usage && exit 0
+fi
+
 for p in $* ; do
     module is-avail $p
     if [ $? -eq 0 ] ; then
